9ebfb9eee4fa3d70525a926a4397da0419bfb286
1abobik1 <dima15042004@mail.ru>
2024-10-05 21:40:03 +0300

    da

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -23,7 +23,7 @@ $api.interceptors.response.use((config) => {
     if (error.response.status == 401 && error.config && !error.config._isRetry) {
         originalRequest._isRetry = true;
         try {
-            const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {withCredentials: true})
+            const response = await axios.get<AuthResponse>(`${API_URL}/refresh/`, {withCredentials: true})
             localStorage.setItem('token', response.data.accessToken);
             return $api.request(originalRequest);
         } catch (e) {
