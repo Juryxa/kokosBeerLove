36b1165c5a91572f4b887fb166cd4c395d4ae55a
Juryxa <slavochka_kondratev80@mail.ru>
2024-10-06 13:45:41 +0300

    some

--- server/auth_microservice/auth_microservice_core/settings.py
+++ server/auth_microservice/auth_microservice_core/settings.py
@@ -109,10 +109,9 @@ WSGI_APPLICATION = 'auth_microservice_core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kokos',
+        'NAME': 'fc_kokoc_db',
         'USER': 'postgres',
-        'PASSWORD': 'dima15042004',
-        'PASSWORD': 'Maksim1516',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
