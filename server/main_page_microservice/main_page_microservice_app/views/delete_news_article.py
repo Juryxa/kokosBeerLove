918682d39c9f3018434c4ff676a650145519771e
1abobik1 <dima15042004@mail.ru>
2024-10-06 23:18:09 +0300

    add new code

--- server/main_page_microservice/main_page_microservice_app/views/delete_news_article.py
+++ server/main_page_microservice/main_page_microservice_app/views/delete_news_article.py
@@ -4,33 +4,35 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.core.files.storage import default_storage
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import authentication_classes, permission_classes
 
 from ..models import NewsArticle
 
 @swagger_auto_schema(
     method='delete',
-    operation_description="Удаление новости по ID",
+    operation_description="Удаление новости по ID (только для администраторов)",
     manual_parameters=[
         openapi.Parameter('article_id', openapi.IN_PATH, description="ID статьи", type=openapi.TYPE_INTEGER),
     ],
     responses={
         204: openapi.Response(description="Новость успешно удалена"),
+        403: openapi.Response(description="Нет прав на удаление"),
         404: openapi.Response(description="Новость не найдена"),
     }
 )
 @api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
 def delete_news_article(request, article_id):
-    try:
-        # Поиск новости по id
-        article = NewsArticle.objects.get(id=article_id)
+    # Проверка, что пользователь администратор
+    if not request.user.is_superuser:
+        return Response({'error': 'Доступ запрещен. Только администраторы могут удалять новости.'}, status=status.HTTP_403_FORBIDDEN)
 
-        # Если у статьи есть изображение, удаляем его
+    try:
+        article = NewsArticle.objects.get(id=article_id)
         if article.image:
             default_storage.delete(article.image.path)
-
-        # Удаляем новость из базы данных
         article.delete()
-
         return Response(status=status.HTTP_204_NO_CONTENT)
     except NewsArticle.DoesNotExist:
         return Response({'error': 'Новость не найдена'}, status=status.HTTP_404_NOT_FOUND)
--- server/main_page_microservice/main_page_microservice_core/settings.py
+++ server/main_page_microservice/main_page_microservice_core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jhllb__6t#(^)qf9j!m!36$e@qq&y$f0u1^ca6wogzx$&iw)@r'
+SECRET_KEY = 'django-insecure-jhllb__6t#(^)qf9j!m!36$e@qq&y$f0u1^ca6wogzx$&iw)@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -99,6 +99,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'main_page_microservice_core.wsgi.application'
 
 
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -132,7 +133,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -163,6 +163,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',  # Для открытых API
+        #'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
@@ -174,9 +175,15 @@ SPECTACULAR_SETTINGS = {
     'VERSION': '1.0.0',
 }
 
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=60),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+}
+
 CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
-
-import os
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
