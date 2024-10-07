10d2d790a72b9d2cc51c0e06a13e8ac739232e59
Juryxa <slavochka_kondratev80@mail.ru>
2024-10-07 17:31:24 +0300

    -

--- client/src/pages/Matches.tsx
+++ client/src/pages/Matches.tsx
@@ -9,31 +9,29 @@ import Carousel from '../components/Carousel';
 import ShopPreview from '../components/ShopPreview';
 
 
-
-
 const Matches: FC = () => {
     return (
         <div style={{height: '100%'}}>
-            <Header />
+            <Header/>
             <div className='container'>
-            <h1>Матчи</h1>
+                <h1>Матчи</h1>
 
-            <div className='content'>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate temporibus,
-                id in harum aspernatur hic fugiat exercitationem porro magnam obcaecati voluptatibus!
-                Explicabo recusandae voluptatum molestiae inventore. Laborum laboriosam doloribus, quae
-                totam dolor voluptatem cupiditate ratione provident nostrum deserunt natus minus pariatur,
-                ducimus, iusto accusantium officiis! Corporis ipsum magnam laborum provident unde possimus
-                quaerat, error inventore porro nostrum quisquam commodi voluptas. Ipsa nihil maiores fugit
-                consectetur. Possimus nam ratione veniam quasi tempora non sunt facilis. Quos sit quae impedit
-                 Voluptate inventore alias hic nobis, cum, vero nostrum quaerat aut consequuntur nesciunt vo
-                 luptates, fugiat assumenda aliquid ratione ut totam quis mollitia quas?
-                 </div>
-                 
-                 
-                 </div>
-                 
-       <Footer/>
+                <div className='content'>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate temporibus,
+                    id in harum aspernatur hic fugiat exercitationem porro magnam obcaecati voluptatibus!
+                    Explicabo recusandae voluptatum molestiae inventore. Laborum laboriosam doloribus, quae
+                    totam dolor voluptatem cupiditate ratione provident nostrum deserunt natus minus pariatur,
+                    ducimus, iusto accusantium officiis! Corporis ipsum magnam laborum provident unde possimus
+                    quaerat, error inventore porro nostrum quisquam commodi voluptas. Ipsa nihil maiores fugit
+                    consectetur. Possimus nam ratione veniam quasi tempora non sunt facilis. Quos sit quae impedit
+                    Voluptate inventore alias hic nobis, cum, vero nostrum quaerat aut consequuntur nesciunt vo
+                    luptates, fugiat assumenda aliquid ratione ut totam quis mollitia quas?
+                </div>
+
+
+            </div>
+
+            <Footer/>
         </div>
     );
 };
