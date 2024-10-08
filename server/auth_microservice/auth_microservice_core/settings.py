19c79688ab20ff266324a28df5a48905ae4b31f3
1abobik1 <dima15042004@mail.ru>
2024-10-08 23:31:25 +0300

    fix bug in verify-email

--- server/auth_microservice/auth_microservice_core/settings.py
+++ server/auth_microservice/auth_microservice_core/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+from datetime import timedelta
+
 from dotenv import load_dotenv
 from pathlib import Path
 from decouple import config
