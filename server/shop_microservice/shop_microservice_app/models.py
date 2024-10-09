30e69c7da9a228fc9a01591aa26b278db9c99180
1abobik1 <dima15042004@mail.ru>
2024-10-10 02:09:39 +0300

    add redis

--- server/shop_microservice/shop_microservice_app/models.py
+++ server/shop_microservice/shop_microservice_app/models.py
@@ -5,7 +5,7 @@ from django.contrib.postgres.fields import ArrayField
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField
     url_images = ArrayField(
         models.CharField(max_length=500), blank=True, default=list
     )  # Поле для массива URL изображений
--- server/shop_microservice/shop_microservice_core/settings.py
+++ server/shop_microservice/shop_microservice_core/settings.py
@@ -191,3 +191,13 @@ SIMPLE_JWT = {
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': config('JWT_SIGNING_KEY'),
 }
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/3',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
