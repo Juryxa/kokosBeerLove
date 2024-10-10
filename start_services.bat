08745003730cec4afce375c6a27784574231fe87
Vyacheslav <101479422+Juryxa@users.noreply.github.com>
2024-10-10 18:53:50 +0300

    Delete start_services.bat

--- start_services.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-start "auth" cmd /k "cd C:\Users\79259\WebstormProjects\kokos\server\auth_microservice && python manage.py runserver 8000"
-start "match" cmd /k "cd C:\Users\79259\WebstormProjects\kokos\server\match_microservice && python manage.py runserver 8002"
-start "news" cmd /k "cd C:\Users\79259\WebstormProjects\kokos\server\news_microservice && python manage.py runserver 8001"
-start "shop" cmd /k "cd C:\Users\79259\WebstormProjects\kokos\server\shop_microservice && python manage.py runserver 8003"
-start "kokoc_team" cmd /k "cd C:\Users\79259\WebstormProjects\kokos\server\kokoc_team_microservice && python manage.py runserver 8004"
-
-pause
