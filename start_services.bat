9446091b9fdf6bfd0dd453c832c931f07c969b29
Maksim <maksimmihaas@gmail.com>
2024-10-10 14:54:25 +0300

    v1

--- start_services.bat
+++ start_services.bat
@@ -1,7 +1,7 @@
 @echo off
-start "auth" cmd /k "cd C:\Users\79259\WebstormProjects\kokos\server\auth_microservice && python manage.py runserver 8000"
-start "match" cmd /k "cd C:\Users\79259\WebstormProjects\kokos\server\match_microservice && python manage.py runserver 8002"
-start "news" cmd /k "cd C:\Users\79259\WebstormProjects\kokos\server\news_microservice && python manage.py runserver 8001"
-start "shop" cmd /k "cd C:\Users\79259\WebstormProjects\kokos\server\shop_microservice && python manage.py runserver 8003"
+start "auth" cmd /k "cd C:\Users\maksi\PycharmProjects\kokosBeerLove\server\auth_microservice && python manage.py runserver 8000"
+start "match" cmd /k "cd C:\Users\maksi\PycharmProjects\kokosBeerLove\server\match_microservice && python manage.py runserver 8002"
+start "news" cmd /k "cd C:\Users\maksi\PycharmProjects\kokosBeerLove\server\news_microservice && python manage.py runserver 8001"
+start "shop" cmd /k "cd C:\Users\maksi\PycharmProjects\kokosBeerLove\server\shop_microservice && python manage.py runserver 8003"
 
 pause
