d9d7a50185a9259be85877bdd65815f4ddce4b49
1abobik1 <dima15042004@mail.ru>
2024-10-10 21:02:22 +0300

    add all handlers for the match

--- server/match_microservice/match_microservice_app/views/heandlers_match/create_match.py
+++ server/match_microservice/match_microservice_app/views/heandlers_match/create_match.py
@@ -4,10 +4,8 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 from rest_framework_simplejwt.authentication import JWTTokenUserAuthentication
 from rest_framework.permissions import IsAuthenticated
 from ...serializers import MatchCreateSerializer
-from ...models import Match
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
-import json
 
 
 @swagger_auto_schema(
@@ -21,6 +19,7 @@ import json
         403: openapi.Response(description="Нет прав доступа"),
     }
 )
+
 @api_view(['POST'])
 @authentication_classes([JWTTokenUserAuthentication])
 @permission_classes([IsAuthenticated])
@@ -30,31 +29,7 @@ def create_match(request):
         return Response({'error': 'У вас нет прав для создания матчей'}, status=status.HTTP_403_FORBIDDEN)
 
     serializer = MatchCreateSerializer(data=request.data)
-
     if serializer.is_valid():
-        # Приведение названия команды к нижнему регистру
-        team_away_name = serializer.validated_data.get('team_away_name').lower()
-        team_away_logo_url = serializer.validated_data.get('team_away_logo_url')
-
-        # Формируем JSON для хранения в поле team_away_info
-        team_away_info = json.dumps({
-            "name": team_away_name,
-            "logo_url": team_away_logo_url
-        })
-
-        # Создаем матч с данными о гостевой команде в формате JSON
-        match = Match.objects.create(
-            team_home=serializer.validated_data.get('team_home'),
-            team_away_info=team_away_info,
-            score_home=serializer.validated_data.get('score_home'),
-            score_away=serializer.validated_data.get('score_away'),
-            location=serializer.validated_data.get('location'),
-            division=serializer.validated_data.get('division'),
-            video_url=serializer.validated_data.get('video_url'),
-            match_date=serializer.validated_data.get('match_date'),
-            match_time=serializer.validated_data.get('match_time')
-        )
-
+        serializer.save()
         return Response(status=status.HTTP_201_CREATED)
-
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
