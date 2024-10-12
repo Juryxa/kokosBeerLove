bf2a7f8c2c92d74cbd9f6384a9a93d242bfa086e
Vyacheslav <101479422+Juryxa@users.noreply.github.com>
2024-10-12 15:57:16 +0300

    shit

--- client/src/components/CreateVideoFrame.tsx
+++ client/src/components/CreateVideoFrame.tsx
@@ -1,18 +1,17 @@
-/* eslint-disable jsx-a11y/iframe-has-title */
 import React from 'react';
 
 interface IframeType {
-    oid: string; 
-    id: string;  
-    hd?: number;           
-    width?: number;       
+    oid: string;
+    id: string;
+    hd?: number;
+    width?: number;
     height?: number;
-    autoplay?: number;     
+    autoplay?: number;
 }
 
 const CreateVideoFrame: React.FC<IframeType> = ({ oid, id, hd = 2, width = 853, height = 480,autoplay }) => {
     const iframeSrc = `https://vk.com/video_ext.php?oid=${oid}&id=${id}&hd=${hd}&autoplay=${autoplay ? 1 : 0}&js_api=1`;
-    
+
 
     return (
         <div>
@@ -23,14 +22,14 @@ const CreateVideoFrame: React.FC<IframeType> = ({ oid, id, hd = 2, width = 853, 
                 allow="autoplay; encrypted-media; fullscreen; picture-in-picture;"
                 frameBorder="0"
                 allowFullScreen
-                style={{ width:"100%", 
+                style={{ width:"100%",
                     maxWidth: "100%",
                     height: "auto" ,
                     display: "block",
                     margin: "0 auto"}}
             ></iframe>
         </div>
-    );  
+    );
 };
 
 export default CreateVideoFrame;
