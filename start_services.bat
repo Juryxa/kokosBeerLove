e1960bedf6bdbf374f251186672a2f650b83b63c
Maksim <maksimmihaas@gmail.com>
2024-10-14 20:36:42 +0300

    v45643463

--- start_services.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-start "auth" cmd /k "cd C:\Users\maksi\PycharmProjects\kokosBeerLove\server\auth_microservice && python manage.py runserver 8000"
-start "match" cmd /k "cd C:\Users\maksi\PycharmProjects\kokosBeerLove\server\match_microservice && python manage.py runserver 8002"
-start "news" cmd /k "cd C:\Users\maksi\PycharmProjects\kokosBeerLove\server\news_microservice && python manage.py runserver 8001"
-start "shop" cmd /k "cd C:\Users\maksi\PycharmProjects\kokosBeerLove\server\shop_microservice && python manage.py runserver 8003"
-start "kokoc_team" cmd /k "cd C:\Users\maksi\PycharmProjects\kokosBeerLove\server\kokoc_team_microservice && python manage.py runserver 8004"
-
-pause
